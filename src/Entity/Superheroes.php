a2c452387b7f4b657d10775731e86ac5b69a9d3f
moitran <moitran92@gmail.com>
2022-03-29 18:45:11 +0700

    init source + docker-compose local env

---
+++ src/Entity/Superheroes.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Table(name="superheroes")
+ * @ApiResource(
+ *   normalizationContext={"groups" = {"read"}},
+ *   denormalizationContext={"groups" = {"write"}}
+ * )
+ */
+class Superheroes
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     * @Groups({"read"})
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\Column(length=70)
+     * @Assert\NotBlank()
+     * @Groups({"read", "write"})
+     */
+    public string $name;
+
+    /**
+     * @ORM\Column(length=70, unique=true)
+     * @Assert\NotBlank()
+     * @Groups({"read", "write"})
+     */
+    public string $slug;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @Groups({"read", "write"})
+     */
+    public bool $featured = false;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups({"read"})
+     */
+    public ?\DateTime $created_at = null;
+
+
+    /******** METHODS ********/
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Prepersist gets triggered on Insert
+     * @ORM\PrePersist
+     */
+    public function updatedTimestamps()
+    {
+        if ($this->created_at == null) {
+            $this->created_at = new \DateTime('now');
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+}
